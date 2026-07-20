e47d0f2691dae8fccf4c3b821f60514253fed42a
Shaban <emir.sabanovic.23@size.ba>
2026-07-20 16:40:10 +0200

    Stage 2: read endpoint with 404

--- Program.cs
+++ Program.cs
@@ -1,14 +1,48 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var tasks = new List<TaskItem>
+{
+    new TaskItem { Id = 1, Title = "Learn ASP.NET Core", Done = false },
+    new TaskItem { Id = 2, Title = "Build a Minimal API", Done = false },
+    new TaskItem { Id = 3, Title = "Push project to GitHub", Done = false }
+};
+
+//Fist Endpoint
 app.MapGet("/", () => new
 {
     name = "Task API", version = "1.0", endpoints = new[] {"/tasks"}
 });
 
+//Health endpoint
 app.MapGet("/health", ()=> new
 {
     status = "ok"
 });
 
+//GetAll()
+app.MapGet("/tasks",()=>tasks);
+
+//GetById()
+app.MapGet("/tasks/{id}",(int id) =>
+{
+    var task = tasks.FirstOrDefault(t=>t.Id==id);
+
+    if(task is null)
+    {
+        return Results.NotFound(new
+        {
+            error = $"Task {id} not found"
+        });
+    }
+    return Results.Ok(task);
+});
+
 app.Run();
+
+class TaskItem
+{
+    public int Id{get;set;}
+    public string Title{get;set;} = string.Empty;
+    public bool Done{get;set;}
+}
